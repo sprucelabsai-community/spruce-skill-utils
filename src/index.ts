8bfda6869e75784037b9b3a480c8785af2a61533
Taylor Romero <taylorromero@taysmacbookpro.local>
2020-12-03 11:51:39 -0700

    patch: missed export

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,6 @@ export { default as pluginUtil } from './plugin.utility'
 export { ErrorOptions as UtilErrorOptions } from './SpruceError'
 export * from './constants'
 export * from './global.types'
+
+export { default as SettingsService } from './services/SettingsService'
+export * from './services/SettingsService'
